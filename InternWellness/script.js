e5e3748ec3db43a2dd8be647e9406532ca45cc67
jamyangc <jamyangchozom7@gmail.com>
2026-04-16 12:05:10 +0600

    Added cleaned version of project

---
+++ InternWellness/script.js
@@ -0,0 +1,5 @@
+function showPage(id) {
+  document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
+  document.getElementById('page-' + id).classList.add('active');
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
