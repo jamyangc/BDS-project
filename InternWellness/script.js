224d2e597e18d6082b609e2bd03e121f8eca9d1b
jamyangc <jamyangchozom7@gmail.com>
2026-04-22 14:05:20 +0600

    updated the code using if else statement

--- InternWellness/script.js
+++ InternWellness/script.js
@@ -27,13 +27,13 @@ function checkMood(mood) {
     action = `<button onclick="showPage('resources')" class="pema-btn">Listen to Music 🎵</button>`;
   }
 
-  if (mood === "neutral") {
+  else if (mood === "neutral") {
     message = "You're feeling okay, maybe a bit low.";
     suggestion = "A short meditation can help refresh your mind.";
     action = `<button onclick="showPage('resources')" class="pema-btn">Try Meditation 🧘</button>`;
   }
 
-  if (mood === "stressed") {
+  else if (mood === "stressed") {
     message = "Your stress level seems high 😣";
     suggestion = "Take a break and try breathing exercises.";
     action = `<button onclick="showPage('resources')" class="pema-btn">Start Breathing 🌬️</button>`;
@@ -58,3 +58,54 @@ function checkMood(mood) {
   `;
 }
 
+const API_KEY = "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"
+
+let history = []
+
+const win = document.getElementById("chat-win");
+const toggle = document.getElementById("chat-toggle");
+const msgs = document.getElementById("cmsgs");
+const input = document.getElementById("cci");
+const sendBtn= document.getElementById("csb");
+toggle.onclick = () => win.classList.toggle("open");
+document.getElementById("chx").onclick = () => win.classList.remove("open");
+function csq(btn) {send(btn.innerText);}
+sendBtn.onclick = () => {
+  send(input.value);
+  input.value = "";
+};
+async function send(text){
+  if (!text)return;
+  addMsg("user", text);
+  history.push({ role:"user",content: text});
+
+  try{
+  const res = await fetch("https://api.groq.com/openai/v1/chat/completions", {
+    method: "POST",
+    headers: {    "Content-Type": "application/json",
+        "Authorization": "Bearer " + API_KEY
+      },
+    body: JSON.stringify({
+      model: "llama-3.3-70b-versatile",
+      max_tokens:200,
+      messages: [
+        { role: "system", content: "You are a supportive wellbeing assistant for interns oranyone who feels stressed." },
+        ...history
+      ]
+    })
+  });
+  const data = await res.json();
+  const reply = data.choices[0].message.content;
+  history.push({ role: "assistant", content: reply });
+  addMsg("ai", reply);
+  } catch (err) {
+    addMsg("ai", "Error: " + err.message);
+  }
+  }
+function addMsg(role, text) {
+  const div = document.createElement("div");
+  div.className = "cmsg " + (role === "user" ? "user" : "");
+  div.innerHTML = `<div class="cmb">${text} </div>`;
+  msgs.appendChild(div);
+  msgs.scrollTop = msgs.scrollHeight;
+} 
